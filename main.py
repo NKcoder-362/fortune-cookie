6b11fef3f93fa55f0c163681955ebfd0031d7f4a
Nan Holly <nanholly@hotmail.com>
2017-03-21 15:45:20 -0500

    implemented lucky number and random fortune and refresh button

--- main.py
+++ main.py
@@ -15,10 +15,40 @@
 # limitations under the License.
 #
 import webapp2
+import random  # import module so we can use random.randint 
+
+def getRandomFortune():
+	#list of possible fortunes
+	fortunes = [
+	"I see much code in your future.", 
+	"Consider eating more fortune cookies.",
+	"You have tamed the mighty Python, now you must free it onto the Great Spider's Web"
+	]
+
+	#randomly select one of the fortunes
+	index = random.randint(0,2)
+
+	return fortunes[index]
+
+	return "test"
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+	def get(self):
+		header = "<h1>Fortune Cookie</h1>"  # added a header to output
+
+		fortune = "<strong>" + getRandomFortune() + "</strong>"
+		fortune_sentence = "Your fortune: " + fortune
+		fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+		lucky_number = "<strong>" + str(random.randint(1,100)) + "</strong>"# randomly selects integer within range 
+		number_sentence = "Your lucky number: " + lucky_number # add var = a sentence + result of random.randint
+		number_paragraph = "<p>" + number_sentence + "</p>" # new var that puts other var in <p> tags
+		
+		cookie_again_button = "<a href='.''><button>Another cookie plz!</button></a>"
+
+		content = header + fortune_paragraph + number_paragraph + cookie_again_button
+
+		self.response.write(content) # modify - del Hello.., concatenate 2 var's
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
